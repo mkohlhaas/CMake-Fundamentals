4f8987ca9d20d728d6ec99999db93254b9fd2259
Hans-J. Schmid <knock@myopendoor.de>
2025-03-29 19:09:51 +0100

    ch 04

---
+++ 04-Fundamentals-Part-4/add/add/add.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <add/calc_status.h>
+
+namespace Add
+{
+    int add(int l, int r, calc_status &cs) noexcept;
+}
---
+++ 04-Fundamentals-Part-4/subtract/subtract/subtract.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    int subtract_i(int l, int r);
+
+#ifdef __cplusplus
+}
+#endif
